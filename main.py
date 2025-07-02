e1e7841bc0d3bd5f68e39da3643c5e90857b60e8
Pruthviraj Rathod <prithvirathod29884@gmail.com>
2025-07-02 18:04:06 +0530

    /

---
+++ main.py
@@ -0,0 +1,18 @@
+import uvicorn
+from fastapi import FastAPI
+from fastapi.responses import JSONResponse
+from fastapi.staticfiles import StaticFiles
+
+
+app =FastAPI()
+
+
+app.mount("/ststic", StaticFiles(directory="static"),name= "static")
+
+@app.get("/")
+def serve_ui():
+    return FileResponse("static/index.html")
+
+if __name__=="__main__":
+    uvicorn.run(app,host="0.0.0.0",port=8000)
+
