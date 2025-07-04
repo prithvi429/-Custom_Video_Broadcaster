642c568e30c490f5ff1abe87f1227f5f91219351
Pruthviraj Rathod <prithvirathod29884@gmail.com>
2025-07-04 18:19:01 +0530

    updats file

--- main.py
+++ main.py
@@ -13,6 +13,11 @@ app.mount("/ststic", StaticFiles(directory="static"),name= "static")
 def serve_ui():
     return FileResponse("static/index.html")
 
+@app.get("/devices")
+def devices():
+    pass
+    return 0
+
 if __name__=="__main__":
     uvicorn.run(app,host="0.0.0.0",port=8000)
 
